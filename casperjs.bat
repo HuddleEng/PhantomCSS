17e77afd149cbd74380b553c2ede04a225d50473
James Cryer <james.cryer@huddle.com>
2014-05-20 11:52:45 +0100

    Removed small command line discrepancy with the casperjs.bat file

--- casperjs.bat
+++ casperjs.bat
@@ -3,4 +3,4 @@ set CASPER_PATH=%~dp0CasperJs
 set CASPER_BIN=%CASPER_PATH%\bin\
 set PHANTOMJS=%~dp0PhantomJs\phantomjs.exe
 set ARGV=%*
-call "%PHANTOMJS%" "%CASPER_BIN%bootstrap.js" test --casper-path="%CASPER_PATH%" --cli %ARGV%
+call "%PHANTOMJS%" "%CASPER_BIN%bootstrap.js" --casper-path="%CASPER_PATH%" --cli %ARGV%
