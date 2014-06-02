5828681672784c49a3cadd45311863716d0efffa
Jay Merrifield <fracmak@gmail.com>
2014-06-02 16:37:58 -0400

    Only write a failure if we haven't defined a _failures directory

--- phantomcss.js
+++ phantomcss.js
@@ -361,13 +361,13 @@ function compareFiles(baseFile, file) {
 
 								casper.captureSelector(failFile, 'img');
 								console.log('Failure! Saved to', failFile);
-							}
-
-							// Always create non-flattened failure images
-							if (file.indexOf('.diff.png') !== -1) {
-								casper.captureSelector(file.replace('.diff.png', '.fail.png'), 'img');
 							} else {
-								casper.captureSelector(file.replace('.png', '.fail.png'), 'img');
+								// Always create non-flattened failure images
+								if (file.indexOf('.diff.png') !== -1) {
+									casper.captureSelector(file.replace('.diff.png', '.fail.png'), 'img');
+								} else {
+									casper.captureSelector(file.replace('.png', '.fail.png'), 'img');
+								}
 							}
 
 							casper.evaluate(function(){
